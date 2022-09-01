fa7ef682f9619cddcefce25e6712a291de3de3b5
Aniruddh Bhilvare <12004314+uniruddh@users.noreply.github.com>
2022-09-01 12:41:04 +0530

    Update SVPinField.swift

--- SVPinView/Source/Classes/SVPinField.swift
+++ SVPinView/Source/Classes/SVPinField.swift
@@ -11,15 +11,7 @@ import UIKit
 class SVPinField: UITextField {
     var deleteButtonAction: SVPinViewDeleteButtonAction = .deleteCurrentAndMoveToPrevious
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-        if action == #selector(UIResponderStandardEditActions.copy(_:)) ||
-            action == #selector(UIResponderStandardEditActions.cut(_:)) ||
-            action == #selector(UIResponderStandardEditActions.select(_:)) ||
-            action == #selector(UIResponderStandardEditActions.selectAll(_:)) ||
-            action == #selector(UIResponderStandardEditActions.delete(_:)) {
-            
-            return false
-        }
-        return super.canPerformAction(action, withSender: sender)
+        return false
     }
     
     override func deleteBackward() {
